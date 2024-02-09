2fbcae43c169e7d88cb2307bd3952f177e775d07
zavame <zavame@farmingdale.edu>
2024-02-08 23:05:49 -0500

    Add class child of person Add GPA Add getter and setter Fix constructor of Student class Add toString Set gpa and scanner

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -4,8 +4,9 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
+import java.util.Scanner;
 public class DriverClass {
-	
+
 	public static void main(String[] args) {
 		// ToDo 5: Fix the error
 		
@@ -13,11 +14,19 @@ public class DriverClass {
 		
 		// ToDo 7: Add a toString method for Student class
 		
-		Student std1= new Student("James", 20);
-		
+		Student std1= new Student ("James", (short) 20);
+
 		// ToDo 8: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
-		
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter "+std1.getName()+" GPA score: ");
+
+		double gpa = scanner.nextDouble();
+
+		std1.setGPA(gpa);
+
 		System.out.println(std1);
 		
 		// ToDo 9: add comments and explain your code
--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,32 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
+public class Student extends Person {
 	// ToDo 1: Make this class a child of Person
-	
+
 	// ToDo 2: Fix the resulting errors
-	
+
 	// ToDo 3: Add a field for GPA and create setter and getter
-	
+	private double GPA;
 	// ToDo 4: Add comments to your code
 
+    private String address;
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public Student(String name, short age) {
+        super(name, age);;
+        address="";
+    }
+
+    @Override
+    public String toString() {
+        return getName()+" who is "+getAge()+" years old, and lives in "+getAddress()+" has " + getGPA()+" GPA score.";;
+    }
 }
